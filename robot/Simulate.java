52ab6b41ebee39089e8849374830cd4a8c6b20f5
Sean Son <astroson2005@gmail.com>
2023-02-22 09:26:37 -0800

    made some performance adjustments

--- robot/Simulate.java
+++ robot/Simulate.java
@@ -19,8 +19,8 @@ public class Simulate extends JFrame implements MouseListener, MouseMotionListen
     private final int CENTER_X = 500;
     private final int CENTER_Y = 700;
     private final int HEIGHT = (int)(CENTER_Y-convertInchestoPixel(ArmConstants.ORIGIN_HEIGHT));
-    double x_2 = 0;
-    double y_2 = 0;
+    int x_2 = 0;
+    int y_2 = 0;
     int last_X = 0;
     int last_Y = 0;
 
@@ -53,25 +53,24 @@ public class Simulate extends JFrame implements MouseListener, MouseMotionListen
         y_click = y;
         x_coordinate = convertPixeltoInches(CENTER_X - x);
         y_coordinate = convertPixeltoInches(CENTER_Y - y);
-        repaint();
-    }
-    public void paint(Graphics g){
-        super.paint(g);
-        g.setColor(Color.black);   
-        g.setFont(new Font("Bold", 1, 20));
-        g.drawString((Double.toString(x_coordinate) + " " + Double.toString(y_coordinate)), 100, 100);  
         double[] angles = InverseKinematicsUtil.getAnglesFromCoordinates(x_coordinate, y_coordinate, 0, flipped);
         if (!Double.isNaN(angles[0])){
             last_X = (int)x_click;
             last_Y = (int)y_click;
-            y_2 = ArmConstants.LIMB1_LENGTH * Math.cos(Math.toRadians(angles[0]));
-            x_2 = ArmConstants.LIMB1_LENGTH * Math.sin(Math.toRadians(angles[0]));
-            g.drawString((Double.toString(angles[0]) + " " + Double.toString(angles[1]) + " " + Double.toString(angles[2])), 100, 150); 
+            y_2 = (int)(HEIGHT + convertInchestoPixel(ArmConstants.LIMB1_LENGTH * Math.cos(Math.toRadians(angles[0]))));
+            x_2 = (int)(CENTER_X - convertInchestoPixel(ArmConstants.LIMB1_LENGTH * Math.sin(Math.toRadians(angles[0]))));
+            //g.drawString((Double.toString(angles[0]) + " " + Double.toString(angles[1]) + " " + Double.toString(angles[2])), 100, 150); 
         }
-
+        repaint();
+    }
+    public void paint(Graphics g){
+        super.paint(g); 
         g.drawLine(CENTER_X, HEIGHT, CENTER_X, CENTER_Y);
-        g.drawLine(CENTER_X, HEIGHT, CENTER_X - convertInchestoPixel(x_2), HEIGHT + convertInchestoPixel(y_2));
-        g.drawLine(CENTER_X - convertInchestoPixel(x_2), HEIGHT + convertInchestoPixel(y_2), last_X, last_Y);
+        g.drawLine(CENTER_X, HEIGHT, x_2, y_2);
+        g.drawLine(x_2, y_2, last_X, last_Y);
+
+        g.setFont(new Font("Bold", 1, 20)); 
+        g.drawString((Double.toString(x_coordinate) + " " + Double.toString(y_coordinate)), 100, 100); 
 
         //double[] coords = ForwardKinematicsUtil.getCoordinatesFromAngles(angles[0], angles[1], angles[2]);
         //g.drawString((Double.toString(coords[0]) + " " + Double.toString(coords[1])) + " " + Double.toString(coords[2]), 100, 200); 
